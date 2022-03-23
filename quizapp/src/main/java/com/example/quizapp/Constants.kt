0f2156e6e557f83675bd20a569f94a762f3f6d4a
bada <bada7144@gmail.com>
2022-03-23 22:55:39 +0900

    modify quiz app

--- quizapp/src/main/java/com/example/quizapp/Constants.kt
+++ quizapp/src/main/java/com/example/quizapp/Constants.kt
@@ -1,5 +1,8 @@
 package com.example.quizapp
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 // SingleTon 패턴
 object Constants {
     // ExtraData Key
@@ -10,8 +13,7 @@ object Constants {
     fun getQuestions(): ArrayList<Question> {
 
         val questionsList = ArrayList<Question>()
-        
-        // 1
+
         val que1 = Question(
             1, "What country does this flag belong to?",
             R.drawable.ic_flag_of_argentina,
@@ -20,7 +22,6 @@ object Constants {
         )
         questionsList.add(que1)
 
-        // 2
         val que2 = Question(
             2, "What country does this flag belong to?",
             R.drawable.ic_flag_of_australia,
@@ -29,7 +30,6 @@ object Constants {
         )
         questionsList.add(que2)
 
-        // 3
         val que3= Question(
             3, "What country does this flag belong to?",
             R.drawable.ic_flag_of_brazil,
@@ -38,7 +38,6 @@ object Constants {
         )
         questionsList.add(que3)
 
-        // 4
         val que4 = Question(
             4, "What country does this flag belong to?",
             R.drawable.ic_flag_of_belgium,
@@ -47,7 +46,6 @@ object Constants {
         )
         questionsList.add(que4)
 
-        // 5
         val que5 = Question(
             5, "What country does this flag belong to?",
             R.drawable.ic_flag_of_fiji,
@@ -56,7 +54,6 @@ object Constants {
         )
         questionsList.add(que5)
 
-        // 6
         val que6 = Question(
             6, "What country does this flag belong to?",
             R.drawable.ic_flag_of_germany,
@@ -65,7 +62,6 @@ object Constants {
         )
         questionsList.add(que6)
 
-        // 7
         val que7 = Question(
             7, "What country does this flag belong to?",
             R.drawable.ic_flag_of_denmark,
@@ -74,7 +70,6 @@ object Constants {
         )
         questionsList.add(que7)
 
-        // 8
         val que8 = Question(
             8, "What country does this flag belong to?",
             R.drawable.ic_flag_of_india,
@@ -83,7 +78,6 @@ object Constants {
         )
         questionsList.add(que8)
 
-        // 9
         val que9 = Question(
             9, "What country does this flag belong to?",
             R.drawable.ic_flag_of_new_zealand,
@@ -92,14 +86,91 @@ object Constants {
         )
         questionsList.add(que9)
 
+        val que10 = Question(
+            10, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_bangladesh,
+            "Bangladesh", "Belgium",
+            "Germany", "India", 1
+        )
+        questionsList.add(que10)
+
+        val que11 = Question(
+            11, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_russia,
+            "Romania", "Russia",
+            "Philippines", "France", 2
+        )
+        questionsList.add(que11)
+
+        val que12 = Question(
+            12, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_peru,
+            "Austria", "Canada",
+            "Peru", "Denmark", 3
+        )
+        questionsList.add(que12)
+
+        val que13 = Question(
+            13, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_finland,
+            "Greece", "England",
+            "Iceland", "Finland", 4
+        )
+        questionsList.add(que13)
+
+        val que14 = Question(
+            14, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_greece,
+            "Finland", "Greece",
+            "Israel", "Scotland", 2
+        )
+        questionsList.add(que14)
+
+        val que15 = Question(
+            15, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_iraq,
+            "Sudan", "Tajikistan",
+            "Iraq", "Syria", 3
+        )
+        questionsList.add(que15)
+
+        val que16 = Question(
+            16, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_philippines,
+            "Philippines", "Russia",
+            "Poland", "Cuba", 1
+        )
+        questionsList.add(que16)
+
+        val que17 = Question(
+            17, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_israel,
+            "Salvador", "Argentina",
+            "Greece", "Israel", 4
+        )
+
+        val que18 = Question(
+            18, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_iceland,
+            "Finland", "Norway",
+            "Iceland", "England", 3
+        )
+        questionsList.add(que18)
+
+
         /*
+        TODO
          문제 추가해서 랜덤하게 출제되도록 변경하기
          XML, JSON 파일에서 문제 읽어와 각 문제를 Question 객체로 변환한 뒤 리스트에 담기
+         JSON 파일에서 image 값으로 저장소에서의 이미지 절대경로를 지정
          아이들 교육용으로 보호자가 직접 퀴즈를 커스터마이징 해서 아이들이 풀 수 있도록? 사용자간에 공유도 가능하게?
          ex : 새학기 반 친구들 얼굴-이름 외우기 퀴즈 등. 개인적인 퀴즈 문제들
          */
 
+        // 문제 섞기
+        questionsList.shuffle()
 
-        return questionsList
+        // 10개만 추출
+        return ArrayList(questionsList.take(10))
     }
 }
